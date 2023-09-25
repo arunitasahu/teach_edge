823e57492480c1c2b73e75768906dfc5f89ec15b
arunitasahu <arunitasahu2311@gmail.com>
2023-09-26 02:39:50 +0530

    Assignment Page

--- lib/presentation/pages/announcement_page/announcement_page.dart
+++ lib/presentation/pages/announcement_page/announcement_page.dart
@@ -57,7 +57,7 @@ class _AnnouncementPageState extends State<AnnouncementPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Announcement Page'),
+        title: Text('Assignment Page'),
         backgroundColor: Color(0xffb3e5f7), // Set the AppBar background color
       ),
       body: SingleChildScrollView(
